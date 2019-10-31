4be7aedad6f76299f334c12a6426434fbbcec8d3
Miguel319 <miguelp90.319@gmail.com>
2019-10-30 20:09:12 -0400

    Carga de imágenes

--- src/app/componentes/carga/carga.component.ts
+++ src/app/componentes/carga/carga.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ArchivoElem } from "../../modelos/archivo.elem";
+import { CargaImgsService } from "../../servicios/carga-imgs.service";
 
 @Component({
-  selector: 'app-carga',
-  templateUrl: './carga.component.html',
-  styleUrls: ['./carga.component.css']
+  selector: "app-carga",
+  templateUrl: "./carga.component.html",
+  styleUrls: ["./carga.component.css"]
 })
 export class CargaComponent implements OnInit {
+  archivos: ArchivoElem[] = [];
 
-  constructor() { }
+  constructor(private cargaImgsService: CargaImgsService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  cargarImagenes() {
+    this.cargaImgsService.cargarImagenesFirebase(this.archivos); 
   }
-
 }
--- src/app/servicios/carga-imgs.service.ts
+++ src/app/servicios/carga-imgs.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Imagen } from '../modelos/imagen.interface';
+import { ArchivoElem } from '../modelos/archivo.elem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CargaImgsService {
-
   private carpetaImgs = 'img';
   
   constructor(private db: AngularFirestore) { }
 
+  cargarImagenesFirebase(imgs: ArchivoElem[]) {
+    console.log(imgs);
+    
+  }
+  
   private guardarImagen(imagen: Imagen) {
     this.db.collection(`/${this.carpetaImgs}`)
       .add(imagen);
